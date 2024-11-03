ddf6e146cc75600431442b4c9458846ad829e0c0
Iman Syaefulloh <imansyaefulloh@gmail.com>
2024-11-03 22:48:44 +0700

    register user API

---
+++ src/main/java/com/imansyaefulloh/restapi/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.imansyaefulloh.restapi.controller;
+
+import com.imansyaefulloh.restapi.model.RegisterUserRequest;
+import com.imansyaefulloh.restapi.model.WebResponse;
+import com.imansyaefulloh.restapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(
+            path = "/api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
+        userService.register(request);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+}
---
+++ src/main/java/com/imansyaefulloh/restapi/service/UserService.java
@@ -0,0 +1,47 @@
+package com.imansyaefulloh.restapi.service;
+
+import com.imansyaefulloh.restapi.entity.User;
+import com.imansyaefulloh.restapi.model.RegisterUserRequest;
+import com.imansyaefulloh.restapi.repository.UserRepository;
+import com.imansyaefulloh.restapi.security.BCrypt;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Validator validator;
+
+    @Transactional
+    public void register(RegisterUserRequest request) {
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
+        if (constraintViolations.size() > 0) {
+            // error
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        if (userRepository.existsById(request.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already registered");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
+    }
+}
