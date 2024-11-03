98c236cd982fd2a23de12a83e87988c59e030b74
Iman Syaefulloh <imansyaefulloh@gmail.com>
2024-11-03 21:29:24 +0700

    fresh install + dependencies

---
+++ src/main/java/com/imansyaefulloh/restapi/LearnSpringbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.imansyaefulloh.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringbootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imansyaefulloh/restapi/LearnSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imansyaefulloh.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
